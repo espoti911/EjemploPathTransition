a0c13b2ecbcc8968f3824702675852b26a9e07f5
Francisco Manuel Gonzalez Martin <franciscomanuelgonzalezmartin@gmail.com>
2024-10-02 13:55:55 +0200

    Arreglos de calidad de codigo

--- src/main/java/es/ieslosmontecillos/ejemplopathtransition/EjemploPathTransition.java
+++ src/main/java/es/ieslosmontecillos/ejemplopathtransition/EjemploPathTransition.java
@@ -11,26 +11,28 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 /**
+ * Desarrollo de Interfaces
  * Ejercicio de ejemplo para trabajar con animaciones
  * @author Francisco Manuel Gonzalez Martin
+ * @version 2-10-2024
  */
-public class HelloApplication extends Application {
+public class EjemploPathTransition extends Application {
     @Override
     public void start(Stage stage)
     {
-        Circle circle = new Circle(50);
-        Rectangle rectangle = new Rectangle(10, 20);
+        Circle circulo = new Circle(50);
+        Rectangle rectangulo = new Rectangle(10, 20);
         Pane root = new Pane();
         PathTransition transition = new PathTransition();
 
         //Colores
-        circle.setFill(Color.WHITE);
-        circle.setStroke(Color.BLACK);
-        rectangle.setFill(Color.ORANGE);
+        circulo.setFill(Color.WHITE);
+        circulo.setStroke(Color.BLACK);
+        rectangulo.setFill(Color.ORANGE);
 
         //Añadir componentes al contenedor
-        root.getChildren().add(circle);
-        root.getChildren().add(rectangle);
+        root.getChildren().add(circulo);
+        root.getChildren().add(rectangulo);
 
         Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Ejemplo path transition");
@@ -42,19 +44,19 @@ public class HelloApplication extends Application {
             Nota: hacer esto antes de ejecutar la animacion ya que PathTransition no permite modificaciones
             mientras se este ejecutando la animacion
          */
-        circle.setCenterX(scene.getWidth() / 2);
-        circle.setCenterY(scene.getHeight() / 2);
+        circulo.setCenterX(scene.getWidth() / 2);
+        circulo.setCenterY(scene.getHeight() / 2);
 
         //Cuando pulsemos el circulo paramos la animacion
-        circle.setOnMousePressed(_ -> transition.pause());
+        circulo.setOnMousePressed(_ -> transition.pause());
 
         //Cuando dejemos de pulsarlo seguira la animacion
-        circle.setOnMouseReleased(_ -> transition.play());
+        circulo.setOnMouseReleased(_ -> transition.play());
 
         //Configuramos la animacion
         transition.setDuration(Duration.millis(4000));
-        transition.setPath(circle);
-        transition.setNode(rectangle);
+        transition.setPath(circulo);
+        transition.setNode(rectangulo);
         transition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
         transition.setCycleCount(Integer.MAX_VALUE);
 
